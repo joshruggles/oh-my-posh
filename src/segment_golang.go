82d72583ee03a2c9e2c52c060b07e7a78a21889c
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2021-12-13 22:35:42 +0100

    feat(go): parse go.mod file for version

--- src/segment_golang.go
+++ src/segment_golang.go
@@ -1,8 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/mod/modfile"
+)
+
 type golang struct {
 	language
 }
+
+const (
+	ParseModFile Property = "parse_mod_file"
+)
 
 func (g *golang) string() string {
 	return g.language.string()
@@ -15,6 +25,10 @@ func (g *golang) init(props properties, env environmentInfo) {
 		extensions: []string{"*.go", "go.mod"},
 		commands: []*cmd{
 			{
+				regex:      `(?P<version>((?P<major>[0-9]+).(?P<minor>[0-9]+)(.(?P<patch>[0-9]+))?))`,
+				getVersion: g.getVersion,
+			},
+			{
 				executable: "go",
 				args:       []string{"version"},
 				regex:      `(?:go(?P<version>((?P<major>[0-9]+).(?P<minor>[0-9]+)(.(?P<patch>[0-9]+))?)))`,
@@ -22,6 +36,22 @@ func (g *golang) init(props properties, env environmentInfo) {
 		},
 		versionURLTemplate: "[%s](https://golang.org/doc/go%s.%s)",
 	}
+}
+
+func (g *golang) getVersion() (string, error) {
+	if !g.props.getBool(ParseModFile, false) {
+		return "", nil
+	}
+	gomod, err := g.language.env.hasParentFilePath("go.mod")
+	if err != nil {
+		return "", fmt.Errorf("go.mod not found as parent")
+	}
+	contents := g.language.env.getFileContent(gomod.path)
+	file, err := modfile.Parse(gomod.path, []byte(contents), nil)
+	if err != nil {
+		return "", err
+	}
+	return file.Go.Version, nil
 }
 
 func (g *golang) enabled() bool {
